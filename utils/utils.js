f973a7964f7b486ef56bd7033f3b99736f945619
Evgeniia Zubova <ev.zu8ova@yandex.ru>
2021-07-15 09:41:55 +0400

    fix: add required option for name in Joi object

--- utils/utils.js
+++ utils/utils.js
@@ -39,7 +39,7 @@ const findMovieJoiObj = Joi.object().keys({
 const signupJoiObj = Joi.object().keys({
   email: Joi.string().required().email(),
   password: Joi.string().required().min(8),
-  name: Joi.string().min(2).max(30),
+  name: Joi.string().required().min(2).max(30),
 });
 
 const signinJoiObj = Joi.object().keys({
